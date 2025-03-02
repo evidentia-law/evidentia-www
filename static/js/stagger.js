80ae76b75dd5cc224cacf9d0e3df984ee79b0005
Erik Näslund <code@nsld.dev>
2025-03-02 21:48:58 +0100

    Add scroll animation

--- static/js/stagger.js
+++ static/js/stagger.js
@@ -1,41 +1,33 @@
-const CSS_DELAY_VAR = `--_delay`;
-
 window.onload = () => {
+  removeJSCheck();
   setupObservers();
-  //   createDelay();
 };
 
+function removeJSCheck() {
+  const elements = document.querySelectorAll(".no-js");
+  elements.forEach((element) => {
+    element.classList.remove("no-js");
+  });
+}
+
 function setupObservers() {
-  const animationTarget = document.querySelectorAll(".animate-fade-in-up");
-  for (let i = 0; i < animationTarget.length; i++) {
-    observer.observe(animationTarget[i]);
-  }
-}
+  const items = document.querySelectorAll(".stagger-item");
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry, index) => {
+        if (entry.isIntersecting) {
+          entry.target.style.transitionDelay = `${index * 0.2}s`;
+          entry.target.classList.remove(
+            "js-enabled:opacity-0",
+            "js-enabled:translate-y-20",
+          );
+          entry.target.classList.add("opacity-100", "translate-y-0");
+          observer.unobserve(entry.target); // Stop observing after animation
+        }
+      });
+    },
+    { threshold: 0.2 },
+  );
 
-function addDelayToElement(element, index) {
-  const delayClass = `${CSS_DELAY_VAR}:${index};`;
-  element.style = delayClass;
+  items.forEach((item) => observer.observe(item));
 }
-function createDelay() {
-  const staggered = document.querySelectorAll(".animate-fade-in-up");
-
-  staggered.forEach((elements) => {
-    let i = 0;
-    for (child of elements.children) {
-      addDelayToElement(child, i);
-      i++;
-    }
-  });
-}
-
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      for (child of entry.target.children) {
-        child.classList.add("animate-fade-in-up");
-      }
-    } else {
-      //   entry.target.classList.remove("scroll-animation");
-    }
-  });
-});
